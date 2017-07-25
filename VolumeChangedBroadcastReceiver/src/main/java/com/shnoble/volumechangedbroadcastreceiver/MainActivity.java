24cfc26a6e9029864eae416d639706f4df9fb7c8
Shnoble <Shnoble@Shnobleui-MacBook-Pro.local>
2017-07-26 01:03:41 +0900

    Implement the VolumeChangedBroadcastReceiver

--- VolumeChangedBroadcastReceiver/src/main/java/com/shnoble/volumechangedbroadcastreceiver/MainActivity.java
+++ VolumeChangedBroadcastReceiver/src/main/java/com/shnoble/volumechangedbroadcastreceiver/MainActivity.java
@@ -1,13 +1,47 @@
 package com.shnoble.volumechangedbroadcastreceiver;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String VOLUME_CHANGED_ACTION = "android.media.VOLUME_CHANGED_ACTION";
+
+    BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            intent.getExtras();
+            if (TextUtils.equals(action, VOLUME_CHANGED_ACTION)) {
+                Toast.makeText(MainActivity.this, "음량이 변화했습니다", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter filter = new IntentFilter(VOLUME_CHANGED_ACTION);
+        registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        unregisterReceiver(mReceiver);
     }
 }
